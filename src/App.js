806c590862b7d1ff8abde19e489d85dedf1e1890
Justin Suderman <jsuderman1@gmail.com>
2020-12-18 11:39:35 -0800

    created header component with stylesheet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>app component</h1>
+      <Header />
+      
     </div>
   );
 }
