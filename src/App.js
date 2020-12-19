d6b63b99c4060c3be9a5cbdeb78bfe02db974554
Justin Suderman <jsuderman1@gmail.com>
2020-12-19 15:07:40 -0800

    added stats and Feed for main body of app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header/Header";
+import Feed from "./components/Feed/Feed"
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Feed />
       
     </div>
   );
---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Feed.css";
+
+function Feed() {
+    return (
+        <div className="feed">
+            <div className="feed__container">
+
+            </div>
+        </div>
+    )
+}
+
+export default Feed
